bedc37e706e9da66fe130b411722ebc421330b13
jake-utk <jakeryanaddis@gmail.com>
2020-11-25 12:02:49 -0600

    Added Github.js scaffold and installed axios

---
+++ src/Components/Github/Github.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import axios from 'axios'
+
+const Github = () => {
+
+    useEffect(() => {
+        axios
+    }, [])
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Github;
