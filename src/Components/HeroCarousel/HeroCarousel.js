5a1c839eea91748a84e4c866551d73dff7ad7cd0
jake-utk <jakeryanaddis@gmail.com>
2020-11-25 15:57:46 -0600

    Fixed hero image api call, reduced top margin

--- src/Components/HeroCarousel/HeroCarousel.js
+++ src/Components/HeroCarousel/HeroCarousel.js
@@ -6,30 +6,23 @@ import "./HeroCarousel.css";
 const HeroCarousel = () => {
 	const [backgrounds, setBackgrounds] = useState([]);
 
-	const testImages = [
-		"https://images.unsplash.com/photo-1556033681-83abea291a96?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE4NTk0NH0",
-		"https://images.unsplash.com/photo-1537889230356-e49d1df471b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE4NTk0NH0",
-	];
-
 	useEffect(() => {
 		const url = `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&page=1&query=nashville`;
 
 		axios
 			.get(url)
 			.then((res) => {
-				console.log(res);
 				return res.data.results.map((image) => {
-					return image.urls.full;
+					return image.urls.raw;
 				});
 			})
-			// .then((images) => setBackgrounds(images))
+			.then((images) => setBackgrounds(images))
 			.catch(console.error);
 	}, []);
 
 	if (!backgrounds) {
 		return <p className='lazy-loader'>Loading...</p>;
 	}
-	console.log("backgrounds", backgrounds);
 
 	return (
 		<div>
@@ -38,8 +31,7 @@ const HeroCarousel = () => {
 				autoplay={true}
 				autoplayInterval={5000}
 				wrapAround={true}>
-				{testImages.map((background, index) => {
-					console.log("background", background);
+				{backgrounds.map((background, index) => {
 					return (
 						<img
 							src={background}
