f71bb154819d6ab07d589ceadb7f4eeb84733fa0
jake-utk <jakeryanaddis@gmail.com>
2020-11-26 12:22:56 -0600

    Small adjustments over past 12 hours

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import AboutMe from "./Components/AboutMe/AboutMe";
 import Resume from "./Components/Resume/Resume";
 import Contact from "./Components/Contact/Contact";
 import BurgerMenu from "./Components/BurgerMenu/BurgerMenu";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { AppContext } from "./AppContext";
-import Hero from "./Components/Hero/Hero"
+import Hero from "./Components/Hero/Hero";
 
 function App() {
 	const [menuOpenState, setMenuOpenState] = useState(false);
@@ -26,7 +26,7 @@ function App() {
               <h1>*CURRENTLY FOR HIRE*</h1>
 							</Link>
 						</div> */}
-            <Hero />
+						<Hero />
 						<div className='main'>
 							<Route path='/' exact component={Home} />
 							<Route path='/aboutme' component={AboutMe} />
@@ -39,10 +39,14 @@ function App() {
 			</div>
 			<footer>
 				Developed by{" "}
-				<a href='https://github.com/jake-utk' target='_blank'>
+				<a href='https://github.com/jake-utk' target='_blank' rel='noreferrer'>
 					Jake Addis
 				</a>{" "}
-				© 2021. Please hire me!
+				(©2021) using{" "}
+				<a href='https://reactjs.org/' target='_blank' rel='noreferrer'>
+					React
+				</a>
+				. Please hire me!
 			</footer>
 		</div>
 	);
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -27,21 +27,11 @@ const Home = () => {
 					<h3>Thanks for checking out my profile.</h3>
 					<h3>Who I Am:</h3>
 					<p>
-						A dynamic Full Stack Software Engineer with experience building
-						projects in React.js, express, and node. I'm also comfortable in
-						python/django. I am currently seeking a full time position. I am
-						prepared to begin immediately. Contact me ASAP to add a tenacious
-						problem solver who excels in a team environment to your project.
+						I view software engineering as a lifelong journey of learning and challenge that allows those who practice this art the daily opportunity to improve oneself.  I believe that through this medium, dreams and ideas can be brought to life and shared with the widest of audiences.  This mindset of a constant drive to improve and a proven ability to create, along with a history of success within a team environment as well as a tenacious approach to problem solving is what I am able to bring to your team.
 					</p>
-					<h3>Another title here.</h3>
+					<h3>What I Do:</h3>
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt
-						sint reprehenderit quibusdam, quasi ut maxime mollitia temporibus!
-						Sequi dolor ut quia culpa magni. Nesciunt voluptatum autem facere
-						numquam iure! Lorem ipsum dolor, sit amet consectetur adipisicing
-						elit. Beatae libero totam provident vitae aut, voluptates delectus
-						recusandae, itaque dignissimos fugiat, animi tempora reiciendis
-						mollitia nesciunt! Natus nisi nihil perferendis officia?
+						I am a dynamic Full Stack Software Engineer with experience building projects and developing solutions in React.js/Express/Node.js and Python/Django.  
 					</p>
 				</div>
 			</div>
