ef022cce23ce9ef9c4a211c63cbf1ba4da06354c
jake-utk <jakeryanaddis@gmail.com>
2020-11-25 22:25:30 -0600

    Clicking twice now closes the menu, animation still not working.  Need a break from this one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Home from "./Components/Home/Home";
 import AboutMe from "./Components/AboutMe/AboutMe";
@@ -6,27 +6,34 @@ import Resume from "./Components/Resume/Resume";
 import Contact from "./Components/Contact/Contact";
 import BurgerMenu from "./Components/BurgerMenu/BurgerMenu";
 import { Route, Link } from "react-router-dom";
+import { AppContext } from "./AppContext";
 
 function App() {
+	const [menuOpenState, setMenuOpenState] = useState(false);
+
 	return (
 		<div className='App'>
-			<BurgerMenu
-				pageWrapId={"page-wrap"}
-				outerContainerId={"outer-container"}
-			/>
-			<div id='page-wrap'>
-				<div className='logo-header'>
-					<Link to='/resume' className='header-link'>
-						<h1>*CURRENTLY FOR HIRE*</h1>
-					</Link>
-				</div>
-				<div className='main'>
-					<Route path='/' exact component={Home} />
-					<Route path='/aboutme' component={AboutMe} />
-					<Route path='/projects' />
-					<Route path='/resume' component={Resume} />
-					<Route path='/contact' component={Contact} />
-				</div>
+			<div id='outer-container'>
+				<AppContext.Provider value={{ menuOpenState, setMenuOpenState }}>
+					<BurgerMenu
+						pageWrapId={"page-wrap"}
+						outerContainerId={"outer-container"}
+					/>
+					<div id='page-wrap'>
+						<div className='logo-header'>
+							<Link to='/resume' className='header-link'>
+								<h1>*CURRENTLY FOR HIRE*</h1>
+							</Link>
+						</div>
+						<div className='main'>
+							<Route path='/' exact component={Home} />
+							<Route path='/aboutme' component={AboutMe} />
+							<Route path='/projects' />
+							<Route path='/resume' component={Resume} />
+							<Route path='/contact' component={Contact} />
+						</div>
+					</div>
+				</AppContext.Provider>
 			</div>
 			<footer>
 				Developed by{" "}
--- src/Components/BurgerMenu/BurgerMenu.js
+++ src/Components/BurgerMenu/BurgerMenu.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import "./BurgerMenu.css"
-import { Link } from 'react-router-dom'
-import { slide as Menu } from 'react-burger-menu'
+import React, { useContext } from "react";
+import "./BurgerMenu.css";
+import { Link } from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
+import { AppContext } from "../../AppContext";
 
 const BurgerMenu = () => {
-    return (
-        <div>
-            <Menu right>
-                <Link to='/' className='bm-item'>Home</Link>
-                <Link to='/aboutme' className='bm-item'>About Me</Link>
-                <Link to='/projects' className='bm-item'>Projects</Link>
-                <Link to='/resume' className='bm-item'>Resume</Link>
-                <Link to='/contact' className='bm-item'>Contact</Link>
-            </Menu>
-        </div>
-    );
+	const { menuOpenState, setMenuOpenState } = useContext(AppContext);
+
+	function toggleMenu(e) {
+		menuOpenState ? setMenuOpenState(false) : setMenuOpenState(true)
+		// e.preventDefault();
+		// setMenuOpenState(false);
+    }
+    
+    function setOpen() {
+        setMenuOpenState(true)
+    }
+
+	return (
+		<div>
+			<Menu right width={"175px"} isOpen={menuOpenState} onClick={setOpen}>
+				<Link to='/' className='bm-item' onClick={toggleMenu}>
+					Home
+				</Link>
+				<Link to='/aboutme' className='bm-item' isOpen={false} onClick={toggleMenu}>
+					About Me
+				</Link>
+				<Link to='/projects' className='bm-item' onClick={toggleMenu}>
+					Projects
+				</Link>
+				<Link to='/resume' className='bm-item' onClick={toggleMenu}>
+					Resume
+				</Link>
+				<Link to='/contact' className='bm-item' onClick={toggleMenu}>
+					Contact
+				</Link>
+			</Menu>
+		</div>
+	);
 };
 
 export default BurgerMenu;
