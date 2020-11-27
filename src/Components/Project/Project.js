d50014a13db0611aea51866f921660e4435ee937
jake-utk <jakeryanaddis@gmail.com>
2020-11-27 14:44:35 -0600

    Finished axios calls, set up state in App.css, Home.js, and Projects.js.  At this time Project.js is not working.

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,48 +1,25 @@
-import React from 'react';
-import "./Project.css"
+import React, { useEffect } from "react";
+import "./Project.css";
 
-const Project = () => {
-    return (
-			<div className='project parallax-project'>
-				<section>
-					<h2>Project Component</h2>
-					<h3>Project 1</h3>
-					<p>
-						Description of project Lorem ipsum dolor sit, amet consectetur
-						adipisicing elit. At, doloribus explicabo laudantium, quidem dolorum
-						suscipit ratione enim consequuntur laborum error iste magnam
-						dignissimos esse ea! Fuga ducimus pariatur quaerat quos.
-					</p>
-				</section>
-				<section>
-					<h3>Project 2</h3>
-					<p>
-						Description of project Lorem ipsum, dolor sit amet consectetur
-						adipisicing elit. Non eos ex quis mollitia quas provident quo
-						doloremque officiis cum deleniti reprehenderit, dolorum ea
-						inventore! Esse magni itaque corrupti laboriosam quos.
-					</p>
-				</section>
-				<section>
-					<h3>Project 3</h3>
-					<p>
-						Description of project Lorem ipsum, dolor sit amet consectetur
-						adipisicing elit. Non eos ex quis mollitia quas provident quo
-						doloremque officiis cum deleniti reprehenderit, dolorum ea
-						inventore! Esse magni itaque corrupti laboriosam quos.
-					</p>
-				</section>
-				<section>
-					<h3>Project 4</h3>
-					<p>
-						Description of project Lorem ipsum, dolor sit amet consectetur
-						adipisicing elit. Non eos ex quis mollitia quas provident quo
-						doloremque officiis cum deleniti reprehenderit, dolorum ea
-						inventore! Esse magni itaque corrupti laboriosam quos.
-					</p>
-				</section>
-			</div>
-		);
+const Project = ({ project }) => {
+
+	useEffect(() => {
+		console.log("PROJECT COMPONENT HAS RECEIVED: ", project);
+	}, []);
+
+	return (
+		<div className='project'>
+			<section>
+				<h3>Project Name</h3>
+				<p>
+					Description of project Lorem ipsum dolor sit, amet consectetur
+					adipisicing elit. At, doloribus explicabo laudantium, quidem dolorum
+					suscipit ratione enim consequuntur laborum error iste magnam
+					dignissimos esse ea! Fuga ducimus pariatur quaerat quos.
+				</p>
+			</section>
+		</div>
+	);
 };
 
 export default Project;
