0543280c486faa464984126f3c563eabaf766add
jake-utk <jakeryanaddis@gmail.com>
2020-12-26 00:42:45 -0600

    About me scaffolded

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Navigation from '../Components/Navigation'
 import Sidebar from '../Components/Sidebar'
 import Hero from '../Components/Hero'
+import About from '../Components/About'
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +16,7 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navigation toggle={toggle} />
             <Hero />
+            <About />
         </>
     )
 }
