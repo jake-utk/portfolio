7b61a35f26189d31d2b83147df609efe56c28c1d
jake-utk <jakeryanaddis@gmail.com>
2020-12-25 20:06:36 -0600

    First renovations, added videos/pages/images, scaffolded pages index.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Home from "./Components/Home/Home";
-import AboutMe from "./Components/AboutMe/AboutMe";
-import Resume from "./Components/Resume/Resume";
-import Contact from "./Components/Contact/Contact";
-import BurgerMenu from "./Components/BurgerMenu/BurgerMenu";
-import Projects from "./Components/Projects/Projects";
-import { Route } from "react-router-dom";
-import Hero from "./Components/Hero/Hero";
+
 
 function App() {
-  const [menuOpenState, setMenuOpenState] = useState(false);
-  const [image, setImage] = useState();
+
 
   return (
-    <div className="App">
-      <div id="outer-container">
-        <BurgerMenu
-          isOpen={menuOpenState}
-          setIsOpen={setMenuOpenState}
-          pageWrapId={"page-wrap"}
-          outerContainerId={"outer-container"}
-        />
-        <Hero />
-        <div className="main">
-          {/* <Route path='/' image={image} component={Home} /> */}
-          <Route path="/aboutme" component={AboutMe} />
-          <Route path="/projects" componeont={Projects} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/contact" component={Contact} />
-        </div>
-        <Home image={image} />
-        <AboutMe />
-        <Projects setImage={setImage} />
-        <Resume />
-        <Contact />
-      </div>
-      <footer>
-        Developed by{" "}
-        <a href="https://github.com/jake-utk" target="_blank" rel="noreferrer">
-          Jake Addis
-        </a>{" "}
-        (©2020) using{" "}
-        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-          React
-        </a>
-        . Please hire me!
-      </footer>
+    <div>
+  
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home
