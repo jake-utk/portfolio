9db092fa3712a2fd2de4678f07ae74f87249387c
jake-utk <jakeryanaddis@gmail.com>
2021-01-06 12:25:46 -0600

    Initialized aos.  Navbar works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages'
-
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
-      </Switch>
-    </Router>
-  );
+	useEffect(() => {
+		Aos.init({});
+	}, []);
+
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route path='/' component={Home} exact />
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
