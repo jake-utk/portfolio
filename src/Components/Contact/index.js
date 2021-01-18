4936ddeb33c8611a6b1a4c074d6a7f324135b73c
jake-utk <jakeryanaddis@gmail.com>
2021-01-18 04:21:15 -0600

    Contact pic added, centered, responsive

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,43 +1,27 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Button } from "../../Components/Styles";
-import Image from "../../images/IMG_4485.JPG";
+import Image from "../../images/undraw1.svg";
 
 const Container = styled.div`
 	background: #e1e2e2; // platinum
-	bottom: 0;
-	left: 0;
 	min-height: 700px;
 	overflow: hidden;
-	right: 0;
-	top: 0;
 	z-index: 0;
-	padding: 100px 0;
-
-	/* @media screen and (max-width: 780px) {
-		padding: 100px 0;
-	} */
+	padding-top: 5vh;
+	padding-bottom: 5vh;
 `;
 
 const Wrapper = styled.div`
 	margin-left: auto;
 	margin-right: auto;
 	max-width: 1100px;
-	/* padding: 0 24px; */
 	width: 100%;
 	z-index: 1;
 	display: flex;
 	flex-direction: column;
 	height: 100%;
 	justify-content: center;
-
-	@media screen and (max-width: 400px) {
-		height: 80%;
-	}
-
-	@media screen and (max-width: 780px) {
-		// code
-	}
 `;
 
 const Content = styled.div`
@@ -45,14 +29,6 @@ const Content = styled.div`
 	height: 100%;
 	justify-content: center;
 	flex-direction: row;
-
-	@media screen and (max-width: 480px) {
-		padding: 10px;
-	}
-	@media screen and (max-width: 780px) {
-		flex-direction: column;
-		// code
-	}
 `;
 
 const Form = styled.form`
@@ -61,7 +37,7 @@ const Form = styled.form`
 	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
 	display: grid;
 	height: auto;
-	margin: auto;
+	margin-right: 10vw;
 	max-width: 400px;
 	padding: 40px 32px;
 	width: 100%;
@@ -69,6 +45,10 @@ const Form = styled.form`
 
 	@media screen and (max-width: 400px) {
 		padding: 32px 32px;
+	}
+
+	@media screen and (max-width: 890px) {
+		margin-right: 0;
 	}
 `;
 
@@ -99,25 +79,25 @@ const InputTextArea = styled.textarea`
 const ImgContainer = styled.div`
 	height: 100%;
 	max-width: 555px;
+	margin-left: 5vw;
 
-	@media screen and (max-width: 780px) {
+	@media screen and (max-width: 890px) {
 		display: none;
 	}
 `;
 
-const Img = styled.div`
-	background: url(${Image});
-	background-position: 65% 0;
+const Img = styled.img`
 	background-size: cover;
 	border-radius: 4px;
-	height: 550px;
+	height: 600px;
 	width: 400px;
 	margin: auto;
 	max-width: 400px;
-	padding: 40px 32px;
 	z-index: 1;
+	padding-top: -100px;
+	padding-bottom: -100px;
 
-	@media screen and (max-width: 780px) {
+	@media screen and (max-width: 890px) {
 		display: none;
 	}
 `;
@@ -168,7 +148,7 @@ const Contact = () => {
 						<FormButton to=''>Send</FormButton>
 					</Form>
 					<ImgContainer>
-						<Img />
+						<Img src={Image} />
 					</ImgContainer>
 				</Content>
 			</Wrapper>
